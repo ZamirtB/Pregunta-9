9a68f89ac95c758fe2beccfabfed8874a625d8bb
zamirt barrera <61028965+ZamirtB@users.noreply.github.com>
2020-04-18 14:33:14 -0500

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -16,17 +16,20 @@ public class Main {
         ArrayList<Inmueble> registroInmueble = new ArrayList<>();
         ArrayList<Contrato> registroContrato = new ArrayList<>();
         Scanner seleccion = new Scanner(System.in);
-        String respuesta;
+        String opcion;
         int i = 1;
+
         do {
             System.out.println("Elige una opcion");
-            System.out.println("1.- Registro de propietario");
-            System.out.println("2.- Registro del inquilino");
-            System.out.println("3.- Registrao del inmueble");
-            System.out.println("4.- Registro del contrato");
+            System.out.println("1 Registro de propietario");
+            System.out.println("2 Registro del inmueble");
+            System.out.println("3 Registro del inquilino");
+            System.out.println("4 Registro del contrato");
+            System.out.println("5 Salir del registro");
 
-            respuesta = seleccion.next();
-            switch (respuesta) {
+            opcion = seleccion.next();
+
+            switch (opcion) {
                 case "1":
                     System.out.println("Nombre del propietario");
                     String Nombre = seleccion.next();
@@ -42,10 +45,21 @@ public class Main {
                                         "\n Nombre: " + registroPropietario.get(j).getNombre() +
                                         "\n Tipo de inmueble: " + registroPropietario.get(j).getTipo() +
                                         "\n Duracion del contrato (meses): " + registroPropietario.get(j).getTiempo());
-                    respuesta = "0";
+                    opcion = "0";
                     break;
+
                 case "2":
-                    System.out.println("Nombre");
+                    System.out.println("Tipo de inmueble");
+                    String tipoInmueble = seleccion.next();
+                    System.out.println("¿Desea arrendar o vender?");
+                    String tipoContrato = seleccion.next();
+                    Inmueble nuevoInmueble = new Inmueble(tipoInmueble, tipoContrato);
+                    registroInmueble.add(nuevoInmueble);
+                    opcion = "0";
+                    break;
+
+                case "3":
+                    //System.out.println("Nombre");
                     String NombreInquilino = seleccion.next();
                     Inquilino nuevoInquilino = new Inquilino(NombreInquilino);
                     registroInquilino.add(nuevoInquilino);
@@ -54,23 +68,15 @@ public class Main {
                                 null,
                                 "Registro exitoso\n los datos registrados son" +
                                         "\n Nombre: " + registroPropietario.get(j).getNombre());
-                    respuesta = "0";
+                    opcion = "0";
                     break;
-                case "3":
-                    System.out.println("Tipo del inmueble");
-                    String tipoInmueble = seleccion.next();
-                    System.out.println("¿Desea arrendar o vender?");
-                    String tipoContrato = seleccion.next();
-                    Inmueble nuevoInmueble = new Inmueble(tipoInmueble, tipoContrato);
-                    registroInmueble.add(nuevoInmueble);
-                    respuesta = "0";
-                    break;
+
                 case "4":
                     System.out.println("Nombre del Inquilino");
                     String nombreInquilinoContrato = seleccion.next();
                     System.out.println("Nombre del Arrendatario");
                     String nombreArrendadorContrato = seleccion.next();
-                    System.out.println("periocidad del contrato");
+                    System.out.println("periodo del contrato");
                     int tiempoArrendamientoContrato = seleccion.nextInt();
                     Contrato nuevoContrato = new Contrato(nombreInquilinoContrato, nombreInquilinoContrato, tiempoArrendamientoContrato);
                     registroContrato.add(nuevoContrato);
@@ -81,25 +87,30 @@ public class Main {
                                 String str1 = registroPropietario.get(k).getNombre();
                                 String str2 = registroInquilino.get(z).getNombreInquilino();
 
-                                // En las sentencias If-else puedes usar el método contains ()
                                 if (str1.contains(registroPropietario.get(k).getNombre()) && str2.contains(registroInquilino.get(z).getNombreInquilino())) {
-                                    JOptionPane.showMessageDialog(null, "El propietario: " + str1 +  " esta en nuestra base de datos");
-                                    JOptionPane.showMessageDialog(null, "El propietario: " + str2 +  " esta en nuestra base de datos, Felicidades su contrato ha sido exitoso");
+                                    JOptionPane.showMessageDialog(null, "El propietario: " + str1 +  " esta en la base de datos");
+                                    JOptionPane.showMessageDialog(null, "El propietario: " + str2 +  " esta en la base de datos,  Su contrato ha sido un exito");
                                 } else {
-                                    System.out.println("La palabra clave: ejemplo: no se encuentra en la cadena");
-                                }}}
+                                    System.out.println("La palabra clave: ejemplo: no existe en la cadena");
+                                }
+                            }
+                        }
                     } catch (Exception e) {
-                        System.out.println("Something went wrong.");
+                        System.out.println("Algo salio mal");
                     }
-                    respuesta = "0";
+                    opcion = "0";
                     break;
                 default:
                     System.out.println("Opción invalida.");
-                    respuesta = "0";
+                    opcion = "0";
+
+                case "5":
+                    System.exit(0);
+                    break;
 
             }
 
-        } while (respuesta == "0");
+        } while (opcion == "0");
 
     }
 }
